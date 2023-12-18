2ebe4986d68ffb1973227160918031c9039f731b
rouzwelt <rouzweltt@gmail.com>
2023-12-18 20:29:22 +0000

    update features

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -3,13 +3,13 @@ pub(crate) mod cli;
 pub(crate) mod solc;
 pub(crate) mod subgraph;
 
-#[cfg(target_feature = "cli")]
+#[cfg(target_feature = "tokio-full")]
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     cli::main()
 }
 
-#[cfg(not(target_feature = "cli"))]
+#[cfg(not(target_feature = "tokio-full"))]
 #[tokio::main(flavor = "current_thread")]
 async fn main() -> anyhow::Result<()> {
     cli::main()
