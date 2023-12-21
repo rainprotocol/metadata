12dfe5c74ca31cb75ea23b494b3796b18eb76a40
rouzwelt <rouzweltt@gmail.com>
2023-12-21 16:43:38 +0000

    update

--- cli/src/lib.rs
+++ cli/src/lib.rs
@@ -1,9 +1,11 @@
 pub mod meta;
-pub mod solc;
 pub mod subgraph;
 
 #[cfg(target_feature = "cli")]
 pub mod cli;
+
+#[cfg(target_feature = "solc")]
+pub mod solc;
 
 // re-export main types and functionalities
 pub use meta::*;
