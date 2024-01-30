82b125177f53396cc4bc6e6f02dea2adcb1e87a3
thedavidmeister <thedavidmeister@gmail.com>
2024-01-30 15:53:18 +0400

    consolidate nix

--- flake.nix
+++ flake.nix
@@ -10,8 +10,36 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = rainix.pkgs.${system};
+        rust-toolchain = rainix.rust-toolchain.${system};
       in {
-        packages = rainix.packages.${system};
+        packages = {
+          mkBin = (pkgs.makeRustPlatform{
+            rustc = rust-toolchain;
+            cargo = rust-toolchain;
+          }).buildRustPackage {
+            src = ./.;
+            doCheck = false;
+            name = "rain-metadata";
+            cargoLock.lockFile = ./Cargo.lock;
+            # allows for git deps to be resolved without the need to specify their outputHash
+            cargoLock.allowBuiltinFetchGit = true;
+            buildPhase = ''
+              cargo build --release --bin rain-metadata --all-features
+            '';
+            installPhase = ''
+              mkdir -p $out/bin
+              cp target/release/rain-metadata $out/bin/
+            '';
+            buildInputs = with pkgs; [
+              openssl
+            ];
+            nativeBuildInputs = with pkgs; [
+              pkg-config
+            ] ++ lib.optionals stdenv.isDarwin [
+              darwin.apple_sdk.frameworks.SystemConfiguration
+            ];
+          };
+        } // rainix.packages.${system};
         devShells = rainix.devShells.${system};
       }
     );
