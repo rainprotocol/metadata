1962e9c0a2c0e511ec4f4ef96aadf5d4a1522714
ninokeldishvili <ninokeldishvili@gmail.com>
2024-04-18 14:26:43 +0400

    Write test to test handleMetaV1. Getting error 'Could not find a mocked function with the following parameters'

--- subgraph/tests/utils.ts
+++ subgraph/tests/utils.ts
@@ -3,16 +3,20 @@ import { ethereum, Address, BigInt, Bytes } from "@graphprotocol/graph-ts";
 import { newMockEvent } from "matchstick-as";
 
 
-export function createNewMetaV1Event(sender: Address, subject: BigInt, meta: Bytes): MetaV1 {
+export function createNewMetaV1Event(sender: string, subject: BigInt, meta: Bytes): MetaV1 {
   // Create a mock ethereum.Event instance
   const metaV1Event = changetype<MetaV1>(newMockEvent());
-  metaV1Event.parameters =
-    [
-      new ethereum.EventParam("sender", ethereum.Value.fromAddress(sender)),
-      new ethereum.EventParam("sender", ethereum.Value.fromI32(subject)),
-      new ethereum.EventParam("sender", ethereum.Value.fromBytes(meta))
-    ];
+  metaV1Event.parameters = new Array()
+  // let idParam = new ethereum.EventParam("id", ethereum.Value.fromI32(1));
+  let senderParam = new ethereum.EventParam("sender", ethereum.Value.fromAddress(Address.fromString(sender)))
 
+  let subjectParam = new ethereum.EventParam("subject", ethereum.Value.fromUnsignedBigInt(subject));
+  let metaParam = new ethereum.EventParam("meta", ethereum.Value.fromBytes(meta));
+
+
+  // metaV1Event.parameters.push(idParam);
+  metaV1Event.parameters.push(senderParam);
+  metaV1Event.parameters.push(subjectParam);
+  metaV1Event.parameters.push(metaParam);
   return metaV1Event;
-
 }
