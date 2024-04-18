34b4b3e9005062fc6784471ea5428f6ba7cb435c
ninokeldishvili <ninokeldishvili@gmail.com>
2024-04-18 17:50:27 +0400

    Write test Can mock metaBoard function correctly

--- subgraph/tests/metaBoard.test.ts
+++ subgraph/tests/metaBoard.test.ts
@@ -1,17 +1,34 @@
-import { test, assert } from "matchstick-as";
+import { test, assert, createMockedFunction } from "matchstick-as";
 import { handleMetaV1 } from "../src/metaBoard";
 import { createNewMetaV1Event } from "./utils";
-import { Bytes, BigInt, ethereum } from "@graphprotocol/graph-ts";
+import { Bytes, BigInt, ethereum, Address } from "@graphprotocol/graph-ts";
+import { MetaBoard } from "../generated/MetaBoard/MetaBoard";
 
-test("MetaV1Event is handled", () => {
-  const address = "0xc0D477556c25C9d67E1f57245C7453DA776B51cf";
-  const subjectBigInt = BigInt.fromI32(123);
+
+test("Can mock metaBoard function correctly", () => {
+  let contractAddress = Address.fromString("0x89205A3A3b2A69De6Dbf7f01ED13B2108B2c43e7");
   const hexString = "0x123456789abcde";
-  const bytes = Bytes.fromHexString(hexString);
-  let newMetaV1Event = createNewMetaV1Event(address, subjectBigInt, bytes);
+  const meta = Bytes.fromHexString(hexString);
+  createMockedFunction(contractAddress, "hash", "hash(bytes):(bytes32)")
+    .withArgs([ethereum.Value.fromBytes(meta)])
+    .returns([ethereum.Value.fromBytes(meta)]);
 
-  handleMetaV1(newMetaV1Event);
+  let metaBoardContract = MetaBoard.bind(contractAddress);
+  let result = metaBoardContract.hash(meta);
 
-  // assert.fieldEquals("MetaV1", "1","sender", address)
-  assert.equals(ethereum.Value.fromString("1"), ethereum.Value.fromString("1"));
+  assert.equals(ethereum.Value.fromBytes(meta), ethereum.Value.fromBytes(result));
 });
+
+//
+// test("MetaV1Event is handled", () => {
+//   const address = "0xc0D477556c25C9d67E1f57245C7453DA776B51cf";
+//   const subjectBigInt = BigInt.fromI32(123);
+//   const hexString = "0x123456789abcde";
+//   const bytes = Bytes.fromHexString(hexString);
+//   let newMetaV1Event = createNewMetaV1Event(address, subjectBigInt, bytes);
+//
+//   handleMetaV1(newMetaV1Event);
+//
+//   // assert.fieldEquals("MetaV1", "1","sender", address)
+//   assert.equals(ethereum.Value.fromString("1"), ethereum.Value.fromString("1"));
+// });
