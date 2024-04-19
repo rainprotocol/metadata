e0a16cc26c49c7005cde40bd4a37103dfacea73b
ninokeldishvili <ninokeldishvili@gmail.com>
2024-04-19 16:23:35 +0400

    Try to test metav1 event

--- subgraph/tests/metaBoard.test.ts
+++ subgraph/tests/metaBoard.test.ts
@@ -1,12 +1,12 @@
-import { test, assert, createMockedFunction } from "matchstick-as";
+import { test, assert, createMockedFunction, clearStore, describe, afterEach} from "matchstick-as";
 import { handleMetaV1 } from "../src/metaBoard";
-import { createNewMetaV1Event } from "./utils";
+import { createNewMetaV1Event, handleNewMetaV1Events } from "./utils";
 import { Bytes, BigInt, ethereum, Address } from "@graphprotocol/graph-ts";
 import { MetaBoard } from "../generated/MetaBoard/MetaBoard";
 
+let contractAddress = Address.fromString("0x89205A3A3b2A69De6Dbf7f01ED13B2108B2c43e7");
 
 test("Can mock metaBoard function correctly", () => {
-  let contractAddress = Address.fromString("0x89205A3A3b2A69De6Dbf7f01ED13B2108B2c43e7");
   const hexString = "0x123456789abcde";
   const meta = Bytes.fromHexString(hexString);
   createMockedFunction(contractAddress, "hash", "hash(bytes):(bytes32)")
@@ -18,17 +18,36 @@ test("Can mock metaBoard function correctly", () => {
 
   assert.equals(ethereum.Value.fromBytes(meta), ethereum.Value.fromBytes(result));
 });
+describe("Mocked Events", () => {
+  afterEach(() => {
+    clearStore()
+  })
 
-//
-// test("MetaV1Event is handled", () => {
-//   const address = "0xc0D477556c25C9d67E1f57245C7453DA776B51cf";
-//   const subjectBigInt = BigInt.fromI32(123);
-//   const hexString = "0x123456789abcde";
-//   const bytes = Bytes.fromHexString(hexString);
-//   let newMetaV1Event = createNewMetaV1Event(address, subjectBigInt, bytes);
-//
-//   handleMetaV1(newMetaV1Event);
-//
-//   // assert.fieldEquals("MetaV1", "1","sender", address)
-//   assert.equals(ethereum.Value.fromString("1"), ethereum.Value.fromString("1"));
-// });
+  test("Can call mappings with custom events", () => {
+    // Call mappings
+  const address = "0xc0D477556c25C9d67E1f57245C7453DA776B51cf";
+  const subjectBigInt = BigInt.fromI32(123);
+  const hexString = "0x123456789abcde";
+  const bytes = Bytes.fromHexString(hexString);
+  let newMetaV1Event = createNewMetaV1Event(address, subjectBigInt, bytes);
+    const meta = Bytes.fromHexString(hexString);
+    createMockedFunction(contractAddress, "hash", "hash(bytes):(bytes32)")
+      .withArgs([ethereum.Value.fromBytes(meta)])
+      .returns([ethereum.Value.fromBytes(meta)]);
+
+
+    let anotherMetaV1Event = createNewMetaV1Event(
+      "0x89205A3A3b2A69De6Dbf7f01ED13B2108B2c43e7",
+      BigInt.fromI32(456),
+      bytes,
+    )
+
+    handleNewMetaV1Events([newMetaV1Event, anotherMetaV1Event])
+
+    // assert.entityCount(GRAVATAR_ENTITY_TYPE, 2)
+    // assert.fieldEquals(GRAVATAR_ENTITY_TYPE, "0xdead", "displayName", "Gravatar 0xdead")
+    // assert.fieldEquals(GRAVATAR_ENTITY_TYPE, "0xbeef", "displayName", "Gravatar 0xbeef")
+  })
+
+})
+
--- subgraph/tests/utils.ts
+++ subgraph/tests/utils.ts
@@ -1,12 +1,15 @@
 import { MetaV1 } from "../generated/MetaBoard/MetaBoard"; // Update the path as per your file structure
 import { ethereum, Address, BigInt, Bytes } from "@graphprotocol/graph-ts";
 import { newMockEvent } from "matchstick-as";
+import {handleMetaV1} from "../src/metaBoard";
 
 
 export function createNewMetaV1Event(sender: string, subject: BigInt, meta: Bytes): MetaV1 {
   // Create a mock ethereum.Event instance
   const metaV1Event = changetype<MetaV1>(newMockEvent());
   metaV1Event.parameters = new Array()
+  metaV1Event.address = Address.fromString("0xfc34d331cc7a4b461b7ded2f6835a837411d8684")
+
   // let idParam = new ethereum.EventParam("id", ethereum.Value.fromI32(1));
   let senderParam = new ethereum.EventParam("sender", ethereum.Value.fromAddress(Address.fromString(sender)))
 
@@ -19,4 +22,10 @@ export function createNewMetaV1Event(sender: string, subject: BigInt, meta: Byte
   metaV1Event.parameters.push(subjectParam);
   metaV1Event.parameters.push(metaParam);
   return metaV1Event;
+}
+
+export function handleNewMetaV1Events(events: MetaV1[]): void {
+  events.forEach(event => {
+    handleMetaV1(event)
+  })
 }
