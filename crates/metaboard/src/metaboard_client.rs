d78e9f1a48cc02577c256696bb3dd032101b72c5
highonhopium <jks.hardy@gmail.com>
2024-04-22 13:27:37 +0100

    tests

--- crates/metaboard/src/metaboard_client.rs
+++ crates/metaboard/src/metaboard_client.rs
@@ -56,3 +56,96 @@ impl MetaboardSubgraphClient {
         Ok(meta_bytes)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use alloy_primitives::hex::encode;
+    use httpmock::Method::POST;
+    use httpmock::MockServer;
+    use reqwest::Url;
+
+    #[tokio::test]
+    async fn test_get_metabytes_by_hash_success() {
+        let server = MockServer::start_async().await;
+        let url = Url::parse(&server.url("/")).unwrap();
+
+        let hash = [1u8; 32];
+
+        // Mock a successful response
+        server.mock(|when, then| {
+            when.method(POST).path("/").body_contains(encode(hash)); // You need to tailor this to the actual body sent
+            then.status(200).json_body_obj(&{
+                serde_json::json!({
+                    "data": {
+                        "metaV1S": [
+                            {
+                             "meta": "0x01",
+                             "metaHash": "0x00",
+                             "sender": "0x00",
+                             "id": "0x00",
+                             "metaBoard": {
+                                 "id": "0x00",
+                                 "metas": [],
+                                 "address": "0x00",
+                             },
+                             "subject": "0x00",
+                            },
+                            {
+                                "meta": "0x02",
+                                "metaHash": "0x00",
+                                "sender": "0x00",
+                                "id": "0x00",
+                                "metaBoard": {
+                                    "id": "0x00",
+                                    "metas": [],
+                                    "address": "0x00",
+                                },
+                                "subject": "0x00",
+                               }
+                        ]
+                    }
+                })
+            });
+        });
+
+        let client = MetaboardSubgraphClient::new(url);
+
+        let result = client.get_metabytes_by_hash(&hash).await;
+
+        assert!(result.is_ok());
+        let result = result.unwrap();
+        assert_eq!(result.len(), 2);
+        assert_eq!(result[0], vec![1]);
+        assert_eq!(result[1], vec![2]);
+    }
+
+    #[tokio::test]
+    async fn test_get_metabytes_by_hash_empty() {
+        let server = MockServer::start_async().await;
+        let url = Url::parse(&server.url("/")).unwrap();
+
+        // Mock an empty response
+        server.mock(|when, then| {
+            when.method(POST).path("/").body_contains("metahash");
+            then.status(200).json_body_obj(&{
+                serde_json::json!({
+                    "data": {
+                        "metaV1S": []
+                    }
+                })
+            });
+        });
+
+        let client = MetaboardSubgraphClient::new(url);
+        let hash = [0u8; 32];
+
+        let result = client.get_metabytes_by_hash(&hash).await;
+
+        assert!(result.is_err());
+        match result {
+            Err(MetaboardSubgraphClientError::Empty) => (),
+            _ => panic!("Unexpected result: {:?}", result),
+        }
+    }
+}
