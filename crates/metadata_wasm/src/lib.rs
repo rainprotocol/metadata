ef17df76c5838583cf5f105225bf0ae290f7764c
ninokeldishvili <ninokeldishvili@gmail.com>
2024-05-16 12:52:58 +0400

    Build success for metadata_wasm. no wasm compile yet

--- crates/metadata_wasm/src/lib.rs
+++ crates/metadata_wasm/src/lib.rs
@@ -1,13 +1,15 @@
-use serde_cbor::Error;
 use wasm_bindgen::prelude::*;
 use rain_metadata::meta::{
                              RainMetaDocumentV1Item,
                              KnownMagic
                          };
 use serde_wasm_bindgen::to_value;
+use rain_metadata::Error;
+use serde::de::{Deserialize, Deserializer, Visitor};
+use serde::ser::{Serialize, SerializeMap, Serializer};
 
 /// method to cbor decode from given bytes
-#[wasm_bindgen]
+// #[wasm_bindgen]
     pub fn cbor_decode(data: &[u8]) -> Result<Vec<RainMetaDocumentV1Item>, Error> {
         let mut track: Vec<usize> = vec![];
         let mut metas: Vec<RainMetaDocumentV1Item> = vec![];
@@ -24,6 +26,7 @@ use serde_wasm_bindgen::to_value;
         while match serde_cbor::Value::deserialize(&mut deserializer) {
             Ok(cbor_map) => {
                 track.push(deserializer.byte_offset());
+                dbg!(&cbor_map);
                 match serde_cbor::value::from_value(cbor_map) {
                     Ok(meta) => metas.push(meta),
                     Err(error) => Err(Error::SerdeCborError(error))?,
